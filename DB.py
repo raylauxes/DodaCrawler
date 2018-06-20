84c13c66d56ca2034a4eff7762cec0e256d5b43f
n4cl <devn4cl@gmail.com>
2018-06-21 00:39:15 +0900

    companyテーブル作成処理を追加

--- DB.py
+++ DB.py
@@ -3,20 +3,27 @@
 from ConfigParser import SafeConfigParser
 import sqlite3 as sqlite
 
+config = SafeConfigParser()
+config.read('config.ini')
 
-class DB(object):
+
+class DodaDB(object):
     def __init__(self, name):
         self.name = name
 
-    def connect(self):
-        sqlite.connect(self.name)
+    def create_company_table(self):
+        """
+        会社情報テーブルを作成
+        """
+        with sqlite.connect(self.name) as con:
+            sql = config.get("db", "sql_create")
+            c = con.cursor()
+            c.execute(sql)
 
 
 def main():
-    config = SafeConfigParser()
-    config.read('config.ini')
-    db = DB(config.get("db", "name"))
-    db.connect()
+    db = DodaDB(config.get("db", "name"))
+    db.create_company_table()
 
 
 if __name__ == "__main__":
