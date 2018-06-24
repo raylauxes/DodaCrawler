3515763607cbc8303ff687615a620e914fc87762
n4cl <devn4cl@gmail.com>
2018-06-24 22:28:04 +0900

    DB.pyにおいて、コンフィグの読み込みスコープをグローバルからローカルに変更

--- DB.py
+++ DB.py
@@ -3,12 +3,12 @@
 from ConfigParser import SafeConfigParser
 import sqlite3 as sqlite
 
-config = SafeConfigParser()
-config.read('config.ini')
 
 
 class DodaDB(object):
     def __init__(self, name):
+        self.config = SafeConfigParser()
+        self.config.read('config.ini')
         self.name = name
 
     def create_company_table(self):
@@ -16,7 +16,7 @@ class DodaDB(object):
         会社情報テーブルを作成
         """
         with sqlite.connect(self.name) as con:
-            sql = config.get("db", "sql_create")
+            sql = self.config.get("db", "sql_create")
             c = con.cursor()
             c.execute(sql)
 
@@ -25,7 +25,7 @@ class DodaDB(object):
         会社情報テーブルが存在するかチェックする
         """
         with sqlite.connect(self.name) as con:
-            sql = config.get("db", "sql_exist")
+            sql = self.config.get("db", "sql_exist")
             c = con.cursor()
             # 結果集合の1件だけ取り出す
             res = c.execute(sql).fetchone()[0]
@@ -37,12 +37,14 @@ class DodaDB(object):
         会社情報テーブルの行を全削除する
         """
         with sqlite.connect(self.name) as con:
-            sql = config.get("db", "sql_delete")
+            sql = self.config.get("db", "sql_delete")
             c = con.cursor()
             c.execute(sql)
 
 
 def main():
+    config = SafeConfigParser()
+    config.read('config.ini')
     db = DodaDB(config.get("db", "name"))
     if not db.is_exist_table():
         db.create_company_table()
